ed68defc452017100425cae5b66f2c4d2b6509ec
aminabdalla <amin.abdalla@sky.uk>
2017-05-15 23:19:30 +0100

    adds a hierarchical place type AA

--- src/main/scala/Geography.scala
+++ src/main/scala/Geography.scala
@@ -1,4 +1,4 @@
-import apple.laf.JRSUIUtils.Tree
+
 
 sealed trait Place{
 
@@ -9,7 +9,6 @@ sealed trait Place{
 
 case class TopPlace(var loc : Location,var children:List[Place]) extends Place {
   override def isParentOf(p: Place): Boolean = true
-
   override def isChild(p: Place): Boolean = false
 }
 
@@ -19,9 +18,11 @@ case class SubPlace(var loc: Location, var children:List[Place]) extends Place {
     case list => if (list.contains(p)) true else children.find(subPlace => subPlace.isParentOf(p)).isDefined
   }
 
-//  override def isChild(p: Location): Boolean = p match {
-//    case TopPlace(c) => List(parent)
-//    case _ => List(parent) ::: parent.isContainedIn
-//  }
-  override def isChild(p: Place): Boolean = ???
+  override def isChild(p: Place): Boolean = p match {
+    case TopPlace(loc,children) => true
+    case SubPlace(loc,children) => if (children.contains(this)) true
+    else if (children.isEmpty) false
+//    else children.map(c => this.isChild(c)).reduce((l,r) => l || r)
+    else children.find(c => this.isChild(c)).isDefined
+  }
 }
--- src/test/scala/PlaceTest.scala
+++ src/test/scala/PlaceTest.scala
@@ -12,11 +12,23 @@ class PlaceTest extends FunSuite {
     val earth = new TopPlace(loc = earthLoc,List(europe))
 
     assert(earth.isParentOf(germany))
+    assert(earth.isParentOf(europe))
+    assert(europe.isParentOf(germany))
 
   }
 
   test("testChildPlaces") {
-
+    val earthLoc: Location = new Location("Earth", (0, 0))
+    val europeLoc: Location = new Location("Europe", (0, 1))
+    val germanyLoc: Location = new Location("Germany", (0, 2))
+    val franceLoc: Location = new Location("France", (0, 2))
+    val france = new SubPlace(loc = franceLoc,List())
+    val germany = new SubPlace(loc = germanyLoc,List())
+    val europe = new SubPlace(loc = europeLoc,List(germany))
+    val earth = new TopPlace(loc = earthLoc,List(europe))
+    assert(germany.isChild(europe))
+    assert(germany.isChild(earth))
+    assert(!germany.isChild(france))
   }
 
   test("testIsSameAs") {
