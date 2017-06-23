4e8f4ebb6505e57afc9f859aaa53f4f4f0231378
aminabdalla <amin.abdalla@sky.uk>
2017-06-23 15:27:03 +0100

    introduces euclidean distance AA

--- src/main/scala/Geometry.scala
+++ src/main/scala/Geometry.scala
@@ -5,13 +5,15 @@ sealed trait Geometry {
 
 type GeomPoint = (Double,Double)
 
-  def euclideanDistance(g2: Geometry): Double = this.toPointRepresentation._1
+  def euclideanDistance(g2: Geometry): Double = {
+    val term1 = Math.pow((g2.toPointRepresentation._1 - this.toPointRepresentation._1),2)
+    val term2 = Math.pow((g2.toPointRepresentation._2 - this.toPointRepresentation._2),2)
+    Math.sqrt(term1 + term2)
+  }
   def toPointRepresentation: GeomPoint= this match {
     case Geometry.BBOX(ll,ur) => ( (ur._1 - ll._1)/2, (ur._2 - ll._2)/2)
     case Geometry.POINT(p) => p
   }
-
-
 
 }
 
--- src/test/scala/GeometryTest.scala
+++ src/test/scala/GeometryTest.scala
@@ -2,14 +2,18 @@ import org.scalatest.FunSuite
 
 class GeometryTest extends FunSuite {
 
+  val point1 = Geometry.POINT(0,0)
+  val point2 = Geometry.POINT(1,1)
+
   val pointLoc = Geometry.POINT(1,2);
   val bboxLoc = Geometry.BBOX((0,0),(1,1));
   test("point stays point")(assert(pointLoc.toPointRepresentation._1 == 1 &&  pointLoc.toPointRepresentation._2 == 2))
   test("bbox is converted to point, x-ccordinate")(assert(bboxLoc.toPointRepresentation._1 == 0.5))
   test("bbox is converted to point, y-ccordinate")(assert(bboxLoc.toPointRepresentation._2 == 0.5))
 
-  test("testEuclideanDistance") {
-
-  }
+  test("testEuclideanDistanceForward")(assert(point1.euclideanDistance(point2) == 1.4142135623730951))
+  test("testEuclideanDistanceBackward")(assert(point2.euclideanDistance(point1) == 1.4142135623730951))
+  test("testEuclideanDistanceBetweenBoxAndPoint")(assert(point1.euclideanDistance(bboxLoc) == 0.7071067811865476))
+  test("testEuclideanDistanceBetweenPointAndBox")(assert(bboxLoc.euclideanDistance(point1) == 0.7071067811865476))
 
 }
